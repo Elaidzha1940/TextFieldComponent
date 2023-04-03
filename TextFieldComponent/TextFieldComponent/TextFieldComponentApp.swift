4a1cb3a1d5174a8cf65701b7ef86800aec0747e7
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:43:24 +0700

    TextFieldComponent

---
+++ TextFieldComponent/TextFieldComponent/TextFieldComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldComponentApp.swift
+//  TextFieldComponent
+//
+//  Created by Elaidzha Shchukin on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
