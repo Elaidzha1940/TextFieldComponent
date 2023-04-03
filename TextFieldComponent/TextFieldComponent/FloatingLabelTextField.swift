b846f650a15c76f6f493fc39ad8af51b5b1f2196
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:55:42 +0700

    TextFieldComponent + fixed status

---
+++ TextFieldComponent/TextFieldComponent/FloatingLabelTextField.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: TextFieldComponent
+//  File: FloatingLabelTextField.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.04.2023
+//
+//  Status / Decoreted
+//
+//  */
+
+import SwiftUI
+
+struct FloatingLabelTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FloatingLabelTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        FloatingLabelTextField()
+    }
+}
