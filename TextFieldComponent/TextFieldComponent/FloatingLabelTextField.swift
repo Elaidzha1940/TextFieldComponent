44ebe3c0e0ab45976bef0aa194117bc51e7e1576
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:56:31 +0700

    TextFieldComponent + fixed status

--- TextFieldComponent/TextFieldComponent/FloatingLabelTextField.swift
+++ TextFieldComponent/TextFieldComponent/FloatingLabelTextField.swift
@@ -11,14 +11,46 @@
 
 import SwiftUI
 
-struct FloatingLabelTextField: View {
+// MARK: FloatingLabelTextFieldCm |
+
+struct  FloatingLabelTextField: View {
+    
+    let placeholder: String
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .leading) {
+            
+            Text(placeholder)
+                .foregroundColor(.gray)
+                .offset(y: self.text.isEmpty ? 0 : -25)
+                .scaleEffect(self.text.isEmpty ? 1 : 0.9, anchor: .leading)
+                .font(.system(self.text.isEmpty ? .title2 : .title3, design: .rounded))
+            
+            TextField("", text: self.$text)
+                .foregroundColor(.gray)
+        }
+        .padding(.top, self.text.isEmpty ? 0 : 18)
+    //.animation(.default)
+        .padding()
+        .background(
+        RoundedRectangle(cornerRadius: 13)
+            .stroke(text.isEmpty ? .pink.opacity(0.5) : .pink, lineWidth: 1)
+        )
     }
 }
 
+
 struct FloatingLabelTextField_Previews: PreviewProvider {
     static var previews: some View {
-        FloatingLabelTextField()
+        Group {
+            FloatingLabelTextField(placeholder: "Ваша Имя", text: .constant(""))
+            FloatingLabelTextField(placeholder: "Телефон", text: .constant(""))
+            FloatingLabelTextField(placeholder: "Пароль", text: .constant(""))
+        }
+        .previewLayout(.sizeThatFits)
+        .padding()
     }
 }
+
