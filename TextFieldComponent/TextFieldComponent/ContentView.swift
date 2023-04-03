ae0850dd805e24beb92841471629752ad0d0ce7b
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:47:13 +0700

    TextFieldComponent + Fixed status + added a File

--- TextFieldComponent/TextFieldComponent/ContentView.swift
+++ TextFieldComponent/TextFieldComponent/ContentView.swift
@@ -1,26 +1,46 @@
+//  /*
 //
-//  ContentView.swift
-//  TextFieldComponent
+//  Project: TextFieldComponent
+//  File: TextFieldComponent.swift
+//  Created by: Elaidzha Shchukin
+//  Date: on 03.04.2023.
 //
-//  Created by Elaidzha Shchukin on 03.04.2023.
+//  Status / Decoreted
 //
+//  */
 
 import SwiftUI
 
-struct ContentView: View {
+// MARK: TextFieldCm |
+
+struct TextFieldComponent: View {
+    
+    @State private var text: String = ""
+    @State private var text1: String = ""
+    @State private var text2: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 24) {
+            Group {
+                
+                FloatingBorderLabelTextField(placholder: "Ваша Имя", text: self.$text)
+                FloatingBorderLabelTextField(placholder: "Телефон", text: self.$text1)
+                FloatingBorderLabelTextField(placholder: "Пароль", text: self.$text2)
+                
+            }
         }
         .padding()
+       .frame(height: 54)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct TextFieldComponent_Preview: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        self.devices {
+            TextFieldComponent()
+        }
     }
 }
+
+
--- TextFieldComponent/TextFieldComponent/TextFieldComponentApp.swift
+++ TextFieldComponent/TextFieldComponent/TextFieldComponentApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TextFieldComponentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TextFieldComponent()
         }
     }
 }
